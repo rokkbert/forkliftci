0fd9d5938cd14a20d401d5b6d58c5c21fd92e3c9
rokkbert <rokkbert@gmail.com>
2022-11-04 18:34:54 +0100

    Deploy http-vcsim from local registry.

--- vmware/setup.sh
+++ vmware/setup.sh
@@ -1,4 +1,4 @@
-kubectl apply -f ./vmware/vcsim_deployment.yml
+kubectl apply -f ./vmware/vcsim_deployment_http.yml
 
 while ! kubectl get deployment -n konveyor-forklift vcsim; do sleep 5; done
 kubectl wait deployment -n konveyor-forklift vcsim --for condition=Available=True --timeout=180s
